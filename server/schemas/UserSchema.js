81213b23cc158106dde0b6ee57c6dd523cf9a163
Justin Atanasiu <justin.atanasiu@gmail.com>
2020-01-18 23:30:32 +0200

    add initial backend with node.js express and mongodb

---
+++ server/schemas/UserSchema.js
@@ -0,0 +1,36 @@
+
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+var UserSchema = mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
+
+//Encrypt password before storing in DB
+UserSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = User;
---
+++ server/services/userService.js
@@ -0,0 +1,78 @@
+const bcrypt = require('bcrypt');
+const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+const passwordRegexp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{6,}$/;
+const jwtService = require('./jwtService.js');
+const User = require('../schemas/UserSchema');
+const StatusCode = require('../common/StatusCode');
+
+var userService = {};
+
+userService.login = function (body, cb) {
+    const email = body.email
+    const password = body.password
+
+    if (email && isValidEmail(email) && password && isValidPassword(password)) {
+        findUser(email, password, function (statusCode, token) {
+            cb(statusCode, token);
+        });
+    } else {
+        cb(StatusCode.BAD_REQUEST)
+    }
+}
+
+var findUser = function (email, password, cb) {
+    User.findOne({ email: email }).exec(function (err, user) {
+        if (err || !user) {
+            cb(401, { errorCode: 'LOGIN_1' });
+        } else {
+            //Since password is encrypted in DB, we need to use bcrypt.compare to test if the input password is correct for user.
+            bcrypt.compare(password, user.password, function (err, result) {
+                if (result === true) {
+                    const { email, username } = user;
+                    cb(StatusCode.OK, { email, username }, jwtService.generateToken(user.email));
+                } else {
+                    cb(StatusCode.UNAUTHORIZED, { errorCode: 'LOGIN_1' });
+                }
+            })
+        }
+    });
+}
+
+userService.register = function (body, cb) {
+    const email = body.email;
+    const password = body.password;
+    const username = body.username;
+
+    if (email && isValidEmail(email) && password && isValidPassword(password) && username && isValidUsername(username)) {
+        var userData = {
+            email: email,
+            username: username,
+            password: password
+        };
+
+        User.create(userData, function (err, user) {
+            if (err) {
+                cb(StatusCode.CONFLICT, { errorCode: 'REGISTER_1' });
+            } else {
+                const { email, username } = user;
+                cb(StatusCode.OK, { email, username }, jwtService.generateToken(user.email));
+            }
+        });
+    } else {
+        cb(StatusCode.BAD_REQUEST);
+    }
+}
+
+var isValidEmail = function (email) {
+    return emailRegexp.test(email);
+}
+
+var isValidUsername = function (username) {
+    return username.length >= 6 || username.length < 20;
+}
+
+var isValidPassword = function (password) {
+    return passwordRegexp.test(password);
+}
+
+module.exports = userService;
